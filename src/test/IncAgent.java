b336411e92c1106b48f08491f015dece4b99d7e3
Gal <galzada570@gmail.com>
2026-01-02 14:27:45 +0200

    Realized IncAgent and PlusAgent

--- src/test/IncAgent.java
+++ src/test/IncAgent.java
@@ -2,7 +2,44 @@ package test;
 
 import test.TopicManagerSingleton.TopicManager;
 
-public class IncAgent {
+public class IncAgent implements Agent {
+    String []m_subs ;
+    String []m_pubs;
+    int value;
 
-    
+    public IncAgent(String []subs, String []pubs) {
+        this.m_subs = subs;
+        this.m_pubs = pubs;
+
+        Topic t1 = TopicManagerSingleton.get().getTopic(m_subs[0]);
+        t1.subscribe(this);
+    }
+
+    @Override
+    public String getName() {
+        return "IncAgent";
+    }
+
+    @Override
+    public void reset() {
+        value = 0;
+    }
+
+    @Override
+    public void callback(String topic, Message msg) {
+        double res = (Double) msg.asDouble;
+        if (Double.isNaN(res))
+            return;
+        value += 1;
+        TopicManagerSingleton
+                .get()
+                .getTopic(m_subs[0])
+                .publish(new Message(res));
+    }
+
+    @Override
+    public void close() {
+
+    }
+
 }
